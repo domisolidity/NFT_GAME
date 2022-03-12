ac309ac49ef71858d3cedfb9a12e7e34715f4445
codecoco <isawmy@nate.com>
2022-03-12 13:40:05 +0900

    feat : 로그인 로그아웃 표시

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./layout.css";
 import TopNav from "./topnav/TopNav";
@@ -6,6 +6,8 @@ import TopNav from "./topnav/TopNav";
 import { useSelector, useDispatch } from "react-redux";
 import { connect } from "../../redux/blockchain/blockchainActions.js";
 import App from "../../App";
+import Login from "../login/Login";
+import Profile from "../login/Profile";
 
 const Layout = () => {
   const dispatch = useDispatch();
@@ -13,6 +15,9 @@ const Layout = () => {
   console.log(blockchain);
 
   const { account, errorMsg } = blockchain;
+
+  const LS_KEY = "login-with-metamask:auth";
+  const [state, setState] = useState({});
 
   const walletConnect = (e) => {
     e.preventDefault();
@@ -27,11 +32,35 @@ const Layout = () => {
     dispatch(connect());
   }, [dispatch]);
 
+  useEffect(() => {
+    // Access token is stored in localstorage
+    const ls = window.localStorage.getItem(LS_KEY);
+    const auth = ls && JSON.parse(ls);
+    setState({ auth });
+  }, []);
+
+  const handleLoggedIn = (auth) => {
+    localStorage.setItem(LS_KEY, JSON.stringify(auth));
+    setState({ auth });
+  };
+
+  const handleLoggedOut = () => {
+    localStorage.removeItem(LS_KEY);
+    setState({ auth: undefined });
+  };
+
+  const { auth } = state;
+
   return (
     <div className="layout">
       {console.log("Layout.js 렌더")}
       <div className="layout__content">
         <TopNav />
+        {auth ? (
+          <Profile auth={auth} onLoggedOut={handleLoggedOut} />
+        ) : (
+          <Login onLoggedIn={handleLoggedIn} />
+        )}
         <div>
           {account ? (
             <div className="layout__wallet">{account}</div>
---
+++ client/src/components/login/Login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+
+const Login = (props) => {
+  const dispatch = useDispatch();
+  const { web3 } = useSelector((state) => state.blockchain);
+
+  const [loading, setLoading] = useState(false); // Loading button state
+
+  const handleAuthenticate = async ({ publicAddress, signature }) =>
+    fetch(`/api/auth`, {
+      body: JSON.stringify({ publicAddress, signature }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    }).then((response) => response.json());
+
+  const handleSignMessage = async ({ publicAddress, nonce }) => {
+    try {
+      const signature = await web3.eth.personal.sign(
+        `I am signing my one-time nonce: ${nonce}`,
+        publicAddress,
+        "" // MetaMask will ignore the password argument here
+      );
+      console.log(signature);
+      return { publicAddress, signature };
+    } catch (err) {
+      throw new Error("You need to sign the message to be able to log in.");
+    }
+  };
+
+  const handleSignup = (publicAddress) =>
+    fetch(`/api/users`, {
+      body: JSON.stringify({ publicAddress }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    }).then((response) => response.json());
+
+  const handleClick = async () => {
+    const coinbase = await web3.eth.getCoinbase();
+    if (!coinbase) {
+      alert("Please activate MetaMask first.");
+      return;
+    }
+
+    const publicAddress = coinbase.toLowerCase();
+    setLoading(true);
+
+    // Look if user with current publicAddress is already present on backend
+    fetch(`/api/users?publicAddress=${publicAddress}`)
+      .then((response) => response.json())
+      // If yes, retrieve it. If no, create it.
+      .then((users) => (users.length ? users[0] : handleSignup(publicAddress)))
+      // Popup MetaMask confirmation modal to sign message
+      .then(handleSignMessage)
+      // Send signature to backend on the /auth route
+      .then(handleAuthenticate)
+      // Pass accessToken back to parent component (to save it in localStorage)
+      .then((auth) => props.onLoggedIn(auth))
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>로그인버튼</button>
+    </div>
+  );
+};
+
+export default Login;
