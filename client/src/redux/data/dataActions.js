2d4f8f67fbe138816ff3e97d812f4b0dbc797cc9
pier101 <85658044+pier101@users.noreply.github.com>
2022-03-13 23:43:14 +0900

    Chore: 리덕스 리듀서 수정

--- client/src/redux/data/dataActions.js
+++ client/src/redux/data/dataActions.js
@@ -21,18 +21,31 @@ const fetchDataFailed = (payload) => {
   };
 };
 
+// const fetchNftSuccess = (payload) =>{
+//   return {
+//     type: "SAVE_NFT_SUCCESS",
+//     payload: payload,
+//   }
+// }
+// const fetchNftFailed = (payload) =>{
+//   return {
+//     type: "SAVE_NFT_Failed",
+//     payload: payload,
+//   }
+// }
+
 export const fetchData = (account) => {
   return async (dispatch) => {
     dispatch(fetchDataRequest());
     try {
-      let allLips = await store.getState().blockchain.lipToken.methods.getLips().call();
-      let allOwnerLips = await store.getState().blockchain.lipToken.methods.getOwnerLips(account).call();
-
+      console.log(account)
+      let myNft = await store.getState().blockchain.nftContract.methods.getMyToken().call({from:account.toString()});
+      console.log(myNft.uri)
+      console.log(myNft.id)
       dispatch(
         fetchDataSuccess({
-          allLips,
-          allOwnerLips,
-        })
+          myNftId:myNft.id,
+          myNftUri:myNft.uri.slice(6)})
       );
     } catch (err) {
       console.log(err);
@@ -40,3 +53,33 @@ export const fetchData = (account) => {
     }
   };
 };
+
+// export const saveNfts = (account,nftInfo) =>{
+//   return async (dispatch)  => {
+//     try {
+//       dispatch(
+//         fetchNftSuccess({
+//           nftInfo
+//         })
+//       )
+//     } catch (error) {
+//       console.log(error)
+//       dispatch(fetchNftFailed("Nft 저장에 실패하였습니다. 민팅과정 재시도 바랍니다."))
+//     }
+//   }
+// }
+
+// export const getMyNft = account =>{
+//   return async (dispatch) =>{
+//     try {
+//       let myNft = await store.getState().blockchain.nftContract.methods.getMyToken().call({from:account});
+//       console.log(myNft.uri)
+//       console.log(myNft.id)
+//       dispatch(
+//         fetchDataSuccess({myNft:myNft})
+//       );
+//     } catch (error) {
+      
+//     }
+//   }
+// }
