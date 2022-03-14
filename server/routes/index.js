81f909d65be77f9270bc477c0f3b80d64614cd67
KimchiChamchi <esm2103@gmail.com>
2022-03-15 02:23:14 +0900

    블록쌓기 게임 라우터 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 const user = require("./users");
 const auth = require("./auth");
+const game = require("./games");
 
 /* 사용자 정보 */
 router.use("/auth", auth);
 
 /* 음악 정보 */
 router.use("/users", user);
+
+/* 게임 */
+router.use("/games", game);
 
 module.exports = router;
