f93e2db58802209b10eda92a6de6fff986d90383
KimchiChamchi <esm2103@gmail.com>
2022-03-15 23:22:13 +0900

    마켓페이지, 페이지 내 아이템카드 임시기본틀 추가

--- client/src/pages/Market.jsx
+++ client/src/pages/Market.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Box, Button, Grid, GridItem } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import ItemCard from "../components/ItemCard";
 
 const Market = () => {
+  const blockchain = useSelector((state) => state.blockchain);
+  const { account } = blockchain;
+  // 아이템 목록
+  const [gameItems, setGameItems] = useState([]);
+
+  // 아이템 목록 가져오기
+  const getGameItems = async () =>
+    await axios
+      .get(`/api/items/game-items`)
+      .then((res) => setGameItems(res.data));
+
+  useEffect(() => {
+    getGameItems();
+  }, [account]);
+
   return (
     <div>
       {console.log("Market.jsx 렌더")}
       MarketPage
+      <Grid
+        templateRows={`auto`}
+        templateColumns={`repeat(3, 1fr)`}
+        gap={5}
+        justifyItems="center"
+      >
+        {gameItems &&
+          gameItems.map((item) => {
+            return <ItemCard key={item.itemId} item={item} />;
+          })}
+      </Grid>
     </div>
   );
 };
