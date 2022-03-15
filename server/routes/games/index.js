d08cc813a14b34e6f922574b14975af31adb5b3c
KimchiChamchi <esm2103@gmail.com>
2022-03-15 23:34:20 +0900

    랭킹 라우터 추가

--- server/routes/games/index.js
+++ server/routes/games/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const ranking = require("./ranking/ranking");
 const stackingBlocks = require("./stackingBlocks/stackingBlocks");
+
+/* 랭킹 */
+router.use("/ranking", ranking);
 
 /* 블록쌓기게임 */
 router.use("/stacking-blocks", stackingBlocks);
