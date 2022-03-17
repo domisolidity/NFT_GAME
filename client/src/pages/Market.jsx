493312aa6ec1473a4549f9a78453c21aca942bce
pier101 <85658044+pier101@users.noreply.github.com>
2022-03-17 23:35:36 +0900

    Style: 마켓 페이지 레이아웃 설정

--- client/src/pages/Market.jsx
+++ client/src/pages/Market.jsx
@@ -1,41 +1,96 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
-import { Box, Button, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Flex, Image, Button } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import ItemCard from "../components/ItemCard";
+import {nfts} from  "../assets/temporaryData/nfts.js";
+import nftImg from "../assets/GameCoin2.png";
 
 const Market = () => {
+  console.log(nfts);
   const blockchain = useSelector((state) => state.blockchain);
   const { account } = blockchain;
-  // 아이템 목록
-  const [gameItems, setGameItems] = useState([]);
+// ===== 상민 =====
+  // // 아이템 목록
+  // const [gameItems, setGameItems] = useState([]);
+  // // 아이템 목록 가져오기
+  // const getGameItems = async () =>
+  //   await axios
+  //     .get(`/api/items/game-items`)
+  //     .then((res) => setGameItems(res.data));
 
-  // 아이템 목록 가져오기
-  const getGameItems = async () =>
-    await axios
-      .get(`/api/items/game-items`)
-      .then((res) => setGameItems(res.data));
-
-  useEffect(() => {
-    getGameItems();
-  }, [account]);
+  // useEffect(() => {
+  //   getGameItems();
+  // }, [account]);
 
   return (
-    <div>
-      {console.log("Market.jsx 렌더")}
-      MarketPage
+    // ===== 상민 =====
+    // <div>
+    //   {console.log("Market.jsx 렌더")}
+    //   MarketPage
+    //   <Grid
+    //     templateRows={`auto`}
+    //     templateColumns={`repeat(3, 1fr)`}
+    //     gap={5}
+    //     justifyItems="center"
+    //   >
+    //     {gameItems &&
+    //       gameItems.map((item) => {
+    //         return <ItemCard key={item.itemId} item={item} />;
+    //       })}
+    //   </Grid>
+    // </div>
+    <>
+      <Box bg="whiteAlpha.100" h="40vh" align="center" lineHeight="40vh">
+          배너
+      </Box>
+      <Box bg="whiteAlpha.100" h="10vh" align="center" lineHeight="10vh" mt="10">
+          Market Place
+      </Box>
       <Grid
-        templateRows={`auto`}
-        templateColumns={`repeat(3, 1fr)`}
-        gap={5}
-        justifyItems="center"
+        mt="2%"
+        h="85%"
+        padding="0 4vw"
+        templateRows="repeat(6, 1fr)" //세로
+        templateColumns="repeat(5, 1fr)" //가로
+        align="center"
+        gap={1}
       >
-        {gameItems &&
-          gameItems.map((item) => {
-            return <ItemCard key={item.itemId} item={item} />;
-          })}
+        <GridItem bg="whiteAlpha.100" colSpan={1} rowSpan={5}>
+          왼쪽바 <br /> Nft 판매 등록 / 필터 
+        </GridItem>
+        <GridItem bg="whiteAlpha.100" colSpan={4} rowSpan={5} >
+            <Grid templateColumns="repeat(4, 1fr)" ml="5"  gap={10} padding="5">
+
+              {nfts && nfts.map((i,item)=>{
+                return (
+                <Box w="15vw" h="40vh" bg="whiteAlpha.500" key={i}>
+                    <Box h="75%" bg="navy">이미지</Box>
+                    <Flex h="15%" justify="space-between">
+                      <Box  bg="green.400">
+                        ㅈㅈㅈ{item.name}
+                      <Box bg="purple" borderRadius="30">ㅇㅇㅇㅇ{item.grade}</Box> 
+                      </Box>
+                      <Box >
+                        price {item.price}
+                      </Box>
+                    </Flex>
+                    <Box h="10%">
+                        <Link to="/market/:wdwd">
+                      <Button bg="blue.400">
+                          Buy now
+                      </Button>
+                        </Link>
+                    </Box>
+                </Box>
+                )
+              })}
+
+            </Grid>
+        </GridItem>
       </Grid>
-    </div>
+    </>
   );
 };
 
