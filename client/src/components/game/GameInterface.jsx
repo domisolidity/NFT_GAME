daa8e87f87fddc2028581d3e4648541c9b145663
KimchiChamchi <esm2103@gmail.com>
2022-03-19 00:39:42 +0900

    게임마다 공통으로 사용할 녀석들 모듈화

---
+++ client/src/components/game/GameInterface.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const gameList = [
+  {
+    gameTitle: "블록쌓기",
+    requestAddress: "stacking-blocks",
+  },
+  { gameTitle: "테트리스", requestAddress: "tetris" },
+];
+
+const getReqAddress = (gameTitle) => {
+  switch (gameTitle) {
+    case gameList[0].gameTitle:
+      return gameList[0].requestAddress;
+    case gameList[1].gameTitle:
+      return gameList[1].requestAddress;
+    default:
+      break;
+  }
+};
+
+// 남은 기회 가져오기
+const getMyChance = async (account, auth, requestAddress) => {
+  if (!account || !auth) return;
+  await axios
+    .post(`/api/games/${requestAddress}/my-count`, { account: account })
+    .then((res) => {
+      return res.data.gameCount;
+    })
+    .catch((err) => console.log(err));
+};
+
+export default { gameList, getMyChance };
