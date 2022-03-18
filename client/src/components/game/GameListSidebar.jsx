fc6ef41109d5895864e19e9b95135cf4dd7dffe0
KimchiChamchi <esm2103@gmail.com>
2022-03-19 00:37:52 +0900

    게임 변경용 사이드 바

--- client/src/components/game/GameListSidebar.jsx
+++ client/src/components/game/GameListSidebar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Button, Flex } from "@chakra-ui/react";
+import GameInterface from "./GameInterface";
 
 const GameListSidebar = (props) => {
+  // 버튼에 있는 텍스트를 상위 컴포넌트(Game)의 함수selectGame에 전달
   const selectGame = (e) => {
     props.selectGame(e.target.innerHTML);
   };
   return (
     <Flex flexDirection={"column"} m={"0 10px"}>
-      {props.gameList.map((game, index) => (
+      {GameInterface.gameList.map((game, index) => (
         <Button key={index} onClick={selectGame}>
-          {game}
+          {game.gameTitle}
         </Button>
       ))}
     </Flex>
