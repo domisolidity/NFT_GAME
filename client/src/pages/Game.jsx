ad4051097e847fd7ce46a064be1dc49b611ada1b
KimchiChamchi <esm2103@gmail.com>
2022-03-20 02:37:04 +0900

    로그인 안하면 게임 기본 페이지만 보이게 하고 게임 선택은 못하게 수정

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -11,59 +11,55 @@ const Game = () => {
   const blockchain = useSelector((state) => state.blockchain);
   const { account, auth } = blockchain;
 
-  // 실행중인 게임
+  /* 실행중인 게임 */
   const [runningGame, setRunningGame] = useState("");
 
-  // 게임 선택
+  /* 게임 선택 */
   const selectGame = async (selectedGame) => {
+    // 로그인 안 하면 게임 선택 못하게 막기
+    if (!(account && auth)) {
+      alert("로그인 안하시면 게임 안 시켜줄겁니다");
+      return;
+    }
+    // 선택한 게임을 useState에 담아 실행중임을 표시
     setRunningGame(selectedGame);
   };
 
-  // 선택한 게임 컴포넌트 소환
+  /* 선택한 게임 컴포넌트 소환 */
   const settingGame = () => {
     switch (runningGame) {
       case "":
         break;
       case GameInterface.gameList[0].gameTitle:
-        return <StackingBlocks />;
+        return <StackingBlocks runningGame={runningGame} />;
       case GameInterface.gameList[1].gameTitle:
-        return <Tetris />;
+        return <Tetris runningGame={runningGame} />;
 
       default:
         break;
     }
   };
 
-  useEffect(() => {
-    console.log(runningGame);
-  }, [runningGame]);
-
   return (
-    <>
-      {account && auth ? (
-        <Flex>
-          {runningGame != "" ? (
-            <GameListSidebar selectGame={selectGame} />
-          ) : // 실행중인 게임이 없으면 사이드바 표시 안하기
-          null}
-          <Box w={"100%"}>
-            {runningGame == "" ? (
-              // 실행중인 게임이 없을 땐 게임선택칸 크게
-              <Flex justifyContent={"space-evenly"}>
-                {GameInterface.gameList.map((game, index) => (
-                  <GameCard key={index} game={game} selectGame={selectGame} />
-                ))}
-              </Flex>
-            ) : (
-              // 실행중인 게임 있으면 해당게임 컴포넌트 출력
-              settingGame()
-            )}
-          </Box>
-        </Flex>
-      ) : (
-        <div>로그인 좀 하세요</div>
-      )}
-    </>
+    <Flex>
+      {runningGame != "" ? (
+        <GameListSidebar selectGame={selectGame} />
+      ) : // 실행중인 게임이 없으면 사이드바 표시 안하기
+      null}
+      <Box w={"100%"}>
+        {runningGame == "" ? (
+          // 실행중인 게임이 없을 땐 게임선택칸 크게
+          <Flex justifyContent={"space-evenly"}>
+            {GameInterface.gameList.map((game, index) => (
+              <GameCard key={index} game={game} selectGame={selectGame} />
+            ))}
+          </Flex>
+        ) : (
+          // 실행중인 게임 있으면 해당게임 컴포넌트 출력
+          settingGame()
+        )}
+      </Box>
+    </Flex>
   );
 };
 
