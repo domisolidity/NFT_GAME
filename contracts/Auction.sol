46fe785ed2b2a67f3d4f9616189234845d24f2f8
pier101 <85658044+pier101@users.noreply.github.com>
2022-04-06 14:21:33 +0900

    Refactor: 경매 컨트랙트 보완

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -12,10 +12,6 @@ contract Auction{
     uint public endBlock; // 시간값 입력받아 초단위 변환후  15(15sec)로 나눠 블록개수로 변환.
     uint public startBid; // 입찰 시작가
 
-    
-
-
-
     enum State {Started, Running, Ended, Canceled}
     State public auctionState;
     
@@ -23,24 +19,27 @@ contract Auction{
     address payable public highestBidder; // 최종 입찰자
     mapping(address => uint) public bids; 
     uint bidIncrement; // 입찰가 증액량
+    uint id;
 
     //소유자는 경매 참여시 한 번만 가장 높은 BindingBid를 얻을 수 있게
     bool public ownerFinalized = false;
-
     mapping(uint256 => bool) public onAuctioning;
  
-    
-    constructor(address payable eoa, address _nftContractAddres, uint _startBid, uint _remainedBlock,uint _tokenId){
+    event PlaceBidEvent(uint indexed id,address account, uint currentBid,uint highestBindingBid,address highestBidder,uint timestamp);
+    event Step(State step);
+
+    constructor(address _eoa, address _nftContractAddres, uint _startBid, uint _remainedBlock,uint _tokenId){
         nftContractAddres = NftContract(_nftContractAddres);
         address nftTokenOwner = nftContractAddres.ownerOf(_tokenId);
-        require(eoa == nftTokenOwner,"you are not token owner!");
+        require(_eoa == nftTokenOwner,"you are not token owner!");
 
-        owner = eoa;
+        owner = payable(_eoa);
         auctionState = State.Running;
-        
+        id=_tokenId;
         startBid = _startBid; 
         startBlock = block.number;
         endBlock = startBlock + _remainedBlock;
+        
 
         bidIncrement = 1*10**17; // 1*10**17 = 0.1 eth
 
@@ -48,22 +47,22 @@ contract Auction{
     }
     
     modifier notOwner(){
-        require(msg.sender != owner);
+        require(msg.sender != owner,"notOwner Error");
         _;
     }
     
     modifier onlyOwner(){
-        require(msg.sender == owner);
+        require(msg.sender == owner,"onlyOwner Error");
         _;
     }
     
     modifier afterStart(){
-        require(block.number >= startBlock);
+        require(block.number >= startBlock,"afterStart Error");
         _;
     }
     
     modifier beforeEnd(){
-        require(block.number <= endBlock);
+        require(block.number <= endBlock,"beforeEnd Error");
         _;
     }
     
@@ -83,12 +82,15 @@ contract Auction{
     
     
     // 경매 메인 함수
-    function placeBid() public payable notOwner afterStart beforeEnd returns(bool){
+    // 시작가보다 낮게 입찰할 수 없게 >> 일케하니 넣은 금액에서 추가할 떄 시작가보다 높아야되서 소량추가가 안됨
+    // 보완하기
+    function placeBid(uint _timeStamp) public payable notOwner afterStart beforeEnd {
         require(auctionState == State.Running);
         // 보낼 수 있는 최소값
         // require(msg.value > 0.0001 ether);
         
-        uint currentBid = bids[msg.sender] + msg.value;
+        uint currentBid = bids[msg.sender] + msg.value ;
+        require(currentBid > startBid,"not higher than the startBid");
         
         // currentBid는 highBindingBid보다 더 커야됨
         require(currentBid > highestBindingBid);
@@ -99,21 +101,22 @@ contract Auction{
         if (currentBid <= bids[highestBidder]){ // 최고 입찰자 변경 안 되는  경우.
             highestBindingBid = min(currentBid + bidIncrement, bids[highestBidder]);
         }else{ // 최고입찰자 변경될 경우
-             highestBindingBid = min(currentBid, bids[highestBidder] + bidIncrement);
+            if(bids[highestBidder] == 0) {
+                highestBindingBid = min(currentBid, startBid + bidIncrement);
+            } else {
+                highestBindingBid = min(currentBid, bids[highestBidder] + bidIncrement);
+            }
              highestBidder = payable(msg.sender);
         }
-    return true;
+ 
+        emit PlaceBidEvent(id,msg.sender,currentBid,highestBindingBid,highestBidder,_timeStamp);
     }
     
     
-    
+    // 경매 종료 후 정산 함수
     function finalizeAuction(uint _tokenId) public{
-       // 경매 취소 또는 종료 상태
        require(auctionState == State.Canceled || block.number > endBlock); 
-       
-       // 소유자 또는 입찰자만 경매를 완료 가능
        require(msg.sender == owner || bids[msg.sender] > 0);
-       
 
        address payable recipient;
        uint value;
@@ -130,23 +133,39 @@ contract Auction{
                if (msg.sender == highestBidder){
                    recipient = highestBidder;
                    value = bids[highestBidder] - highestBindingBid;
+                   nftContractAddres.safeTransferFrom(owner, highestBidder, id);
                }else{ // 일반 입찰자 경매 정산.
                    recipient = payable(msg.sender);
                    value = bids[msg.sender];
                }
            }
        }
-       
-       // 동일한 수신자에게 여러 번 전송되는 것을 방지하기 위해 수신자의 입찰가 재설정
        bids[recipient] = 0;
-       
-       // 경매 참여 케이스별 정산 실행
        recipient.transfer(value);
        onAuctioning[_tokenId] = false;
     } 
 
     function isAuctionCheck(uint _tokenId) public view returns(bool) {
         return onAuctioning[_tokenId];
+    }
+
+
+    function getInitInfo() external view returns(State,uint,uint,uint,uint,uint){
+        return (auctionState,startBid,bidIncrement,startBlock,block.number,endBlock);
+    }
+    function getStartBid() external view returns(uint){
+        return startBid;
+    }
+    function getBidAmount() external view returns(uint){
+        return bids[msg.sender];
+    }
+
+    function isHighestBidder() external view returns(bool) {
+        if (bids[msg.sender] < startBid) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
--- contracts/AuctionCreator.sol
+++ contracts/AuctionCreator.sol
@@ -2,46 +2,50 @@
 pragma solidity ^0.8.0;
 
 import "./Auction.sol";
-  
+
 // 경매 컨트랙트 생성용 컨트랙트
 contract AuctionCreator {
+  mapping(uint => Auction) public auctionToId;
+  uint private endBlock;
 
-    mapping(uint=>Auction) public auctionToId;
-    uint private endBlock; 
+  struct AuctionNft {
+    address account;
+    uint8 tokenId;
+    uint endBlockNum;
+    uint endTime;
+    Auction auctionContract;
+  }
+  AuctionNft[] public auctionNfts;
 
-    struct AuctionNft{
-        address account;
-        uint8 tokenId;
-        uint endBlockNum;
-        Auction auctionContract;
+  //경매 컨트랙트 생성 함수
+  function createAuction(
+    uint8 _tokenId,
+    address _nftContractAddres,
+    uint _startBid,
+    uint _remainedBlock,
+    uint _expirationTime
+  ) public {
+    Auction newAuction = new Auction(msg.sender, _nftContractAddres, _startBid, _remainedBlock, _tokenId);
+    auctionToId[_tokenId] = newAuction;
 
-    }
-    AuctionNft[] public auctionNfts;
-    
-    //경매 컨트랙트 생성 함수
-    function createAuction( uint8 _tokenId,address _nftContractAddres, uint _startBid,uint _remainedBlock) public{
-        Auction newAuction = new Auction(payable(msg.sender),_nftContractAddres, _startBid,_remainedBlock,_tokenId); 
-        auctionToId[_tokenId] = newAuction;
+    endBlock = block.number + _remainedBlock;
 
-        endBlock = block.number + _remainedBlock;
+    auctionNfts.push(AuctionNft(msg.sender, _tokenId, endBlock, _expirationTime, newAuction));
+  }
 
-        auctionNfts.push(AuctionNft(msg.sender,_tokenId,endBlock,newAuction));
+  function getAuctionAddress(uint _tokenId) public view returns (Auction) {
+    return auctionToId[_tokenId];
+  }
 
-    }
+  function getAuctioningNft() external view returns (AuctionNft[] memory) {
+    return auctionNfts;
+  }
 
-    function getAuctionAddress(uint _tokenId) public view returns(Auction) {
-        return auctionToId[_tokenId];
-    }
-
-    function getAuctioningNft() external view returns(AuctionNft[] memory){
-        return auctionNfts;
-    }
-    function getCurrentBlock() external view returns(uint) {
-        return block.number;
-    }
-  
+  function getCurrentBlock() external view returns (uint) {
+    return block.number;
+  }
 }
- /*
+/*
  ipfs://QmaGTbhBGBfdH6gXLoC4V5GScY1mtxac4Ye6ynA1Djw6mZ
     0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
     91
