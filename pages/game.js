689b26ffd3b6d5be9accea6c1892e55339f6d251
pier101 <85658044+pier101@users.noreply.github.com>
2022-04-08 14:43:14 +0900

    Chore: 프리티어 코드 수정

--- pages/game.js
+++ pages/game.js
@@ -11,8 +11,7 @@ const Game = () => {
   const { account, auth, nftContract } = blockchain;
   const router = useRouter();
 
-  console.log(GameInterface.gameList)
-
+  console.log(GameInterface.gameList);
 
   /* 게임 선택 */
   // const getSelectGame = async (selectedGame) => {
@@ -26,32 +25,29 @@ const Game = () => {
   //   setRunningGame(selectedGame);
   // };
 
-
   const selectGame = async (game) => {
-    console.log("게임", game)
-    console.log(nftContract)
+    console.log("게임", game);
+    console.log(nftContract);
     //홀더 자격 확인
-    const haveToken = await nftContract.methods.haveTokenBool(account).call({ from: account })
+    const haveToken = await nftContract.methods.haveTokenBool(account).call({ from: account });
     if (!haveToken) {
-      alert("Nft를 가지고 있지 않습니다. \n 민팅 후 게임에 참여가 가능합니다.")
+      alert("Nft를 가지고 있지 않습니다. \n 민팅 후 게임에 참여가 가능합니다.");
       return false;
     } else {
       const selectedGame = game.gameUrl;
-      console.log("selectedGame", game.gameUrl)
+      console.log("selectedGame", game.gameUrl);
       if (window.confirm(`${game.description}\n게임을 플레이 하시겠습니까?`))
         if (!(account && auth)) {
           alert("로그인 안하시면 게임 안 시켜줄겁니다");
           return;
         }
       // 선택한 게임을 useState에 담아 실행중임을 표시
-      console.log("selectedGameselectedGame", selectedGame)
+      console.log("selectedGameselectedGame", selectedGame);
 
-      router.push(`game/${selectedGame}`)
+      router.push(`game/${selectedGame}`);
       return true;
     }
-
-  }
-
+  };
 
   return (
     <Flex>
@@ -59,13 +55,14 @@ const Game = () => {
       <Box w={"100%"} minHeight={"400px"} position={`relative`}>
         <Flex justifyContent={"space-evenly"}>
           {/* {console.log(game)} */}
-          {GameInterface.gameList.map((game, index) => (<>
-            <Link href={(() => selectGame(game)) == true ? `/game/${game.gameUrl}` : `/game`} >
-              <a onClick={() => selectGame(game)} style={{ width: '30%', height: '100%' }}>
-                <GameCard key={index} game={game} />
-              </a>
-            </Link>
-          </>
+          {GameInterface.gameList.map((game, index) => (
+            <>
+              <Link href={(() => selectGame(game)) == true ? `/game/${game.gameUrl}` : `/game`}>
+                <a onClick={() => selectGame(game)} style={{ width: "30%", height: "100%" }}>
+                  <GameCard key={index} game={game} />
+                </a>
+              </Link>
+            </>
           ))}
         </Flex>
       </Box>
