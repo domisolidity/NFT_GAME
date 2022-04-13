53d0eb1bb25b66b55c4c40a0c2fd413707852ba8
KimchiChamchi <esm2103@gmail.com>
2022-04-14 00:35:57 +0900

    보물찾기 게임 끝날때 점수 갱신 버그 수정, 게임페이지 디자인 중..

--- components/game/GameCard.jsx
+++ components/game/GameCard.jsx
@@ -3,34 +3,115 @@ import React from "react";
 
 const GameCard = ({ game }) => {
   return (
-    <Box
-      overflow={`hidden`}
-      borderRadius={`15px`}
-      w={`100%`}
-      h={`100%`}
-      position={`relative`}
-    >
-      <Img w={`100%`} src={`./images/game_${game.gameId}.png`} />
-      <Flex
-        w={`100%`}
-        h={`100%`}
-        _hover={{ opacity: `1`, cursor: `pointer` }}
-        transition={`0.5s ease`}
-        opacity={`0`}
-        position={`absolute`}
-        top={`50%`}
-        left={`50%`}
-        transform={`translate(-50%, -50%)`}
-        textAlign={`center`}
-        backgroundColor={`rgba(200,200,200,0.5)`}
-        justifyContent={`center`}
-        alignItems={`center`}
-      >
-        <Text color={`#1e315f`} fontSize={`50px`} fontWeight={`bold`}>
-          {game.gameTitle}
-        </Text>
-      </Flex>
-    </Box>
+    <>
+      <div className="game-card">
+        <img
+          className="game-card-img"
+          src={`./images/game_${game.gameId}.png`}
+        />
+        <div className="game-card-box">
+          <p className="game-card-text">{game.gameTitle}</p>
+        </div>
+        {/* <div className="loader">
+          <span></span>
+        </div> */}
+      </div>
+
+      <style jsx>{`
+        .game-card {
+          display: flex;
+          overflow: hidden;
+          border-radius: 15px;
+          width: 100%;
+          height: 100%;
+          position: relative;
+          color: #1e315f;
+          font-size: 50px;
+          font-weight: bold;
+        }
+        .game-card::before {
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+
+          width: 50%;
+          height: 100%;
+          background: linear-gradient(
+            to top,
+            transparent,
+            rgba(0, 255, 249, 0.4)
+          );
+          background-size: 100px 180px;
+          background-repeat: no-repeat;
+        }
+        .game-card::after {
+          content: "";
+          position: absolute;
+          top: 0%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          width: 20px;
+          height: 20px;
+          background-color: #00fff9;
+          border-radius: 50%;
+          z-index: 10;
+          box-shadow: 0 0 10px #00fff9, 0 0 20px #00fff9, 0 0 30px #00fff9,
+            0 0 40px #00fff9, 0 0 50px #00fff9, 0 0 60px #00fff9,
+            0 0 70px #00fff9, 0 0 80px #00fff9, 0 0 90px #00fff9,
+            0 0 100px #00fff9;
+        }
+        .game-card-img {
+          width: 100%;
+        }
+        .game-card-box {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          transition: 0.5s ease;
+          opacity: 0;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          text-align: center;
+          background-color: rgba(200, 200, 200, 0.5);
+          justify-content: center;
+          align-items: center;
+        }
+        .game-card-box:hover {
+          opacity: 1;
+          cursor: pointer;
+          -webkit-box-reflect: below 10px linear-gradient(#0001, #0004);
+        }
+
+        .loader::after {
+          content: "";
+          position: absolute;
+          top: 0%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          width: 20px;
+          height: 20px;
+          background-color: #00fff9;
+          border-radius: 50%;
+          z-index: 10;
+          box-shadow: 0 0 10px #00fff9, 0 0 20px #00fff9, 0 0 30px #00fff9,
+            0 0 40px #00fff9, 0 0 50px #00fff9, 0 0 60px #00fff9,
+            0 0 70px #00fff9, 0 0 80px #00fff9, 0 0 90px #00fff9,
+            0 0 100px #00fff9;
+        }
+        .loader span {
+          position: absolute;
+          top: 20px;
+          left: 20px;
+          right: 20px;
+          bottom: 20px;
+          /* background: #102626; */
+          background-image: url("./images/game_3.png");
+        }
+      `}</style>
+    </>
   );
 };
 
