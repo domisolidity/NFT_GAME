20c09724cd14e489a926d09eec97f6db1b8be23f
pier101 <85658044+pier101@users.noreply.github.com>
2022-04-19 23:52:19 +0900

    Chore: 전체 코드 부분 변경 적용

--- components/Countdown.jsx
+++ components/Countdown.jsx
@@ -1,51 +1,54 @@
-import { useState,useEffect } from 'react';
-import React from 'react'
+import { useState, useEffect } from "react";
+import React from "react";
 
 const Countdown = (props) => {
-    const endTime = props.remain;
+  const endTime = props.remain;
 
-    const remainTime = endTime - Date.now()
-    const [remainingTime, setRemainingTime] = useState("00:00:00");
-    
-    useEffect(() => {
-        if (remainTime <= 0) return;
-        const intervalId = setInterval(() => {
-            if (remainTime < 1000) {
-                console.log(remainTime)
-                props.setEnd(true);
-                return;
-            }
-            updateRemainingTime();
-        }, 1000);
-        return () => clearInterval(intervalId);
-    },[remainingTime]); 
-    
-    
-    function updateRemainingTime() {
-        const date = new Date(remainTime);
-        const remainSec = ("0"+date.getSeconds()).slice(-2);
-        const remainMin = ("0"+date.getMinutes()).slice(-2);
-        const remainHour = Math.floor(remainTime / 1000 / 60 / 60);
-        const countdown = `${remainHour}:${remainMin}:${remainSec}`
-        setRemainingTime(countdown);
-    }
+  const remainTime = endTime - Date.now();
+  const [remainingTime, setRemainingTime] = useState("00:00:00");
 
-    return (
-        <div style={{marginTop:20}}>
-            {remainTime < 1000 ? <div className='endAuction'>경매 종료</div> : <div>남은 시간 {remainingTime}</div>}
-            <style jsx>{`
-                div{
-                    text-align:center;
-                    color: #df973a;
-                    font-weight: 700;
-                }
-                .endAuction{
-                    color: #999494;
-                }
+  useEffect(() => {
+    if (remainTime <= 0) return;
+    const intervalId = setInterval(() => {
+      if (remainTime < 1000) {
+        console.log(remainTime);
+        props.setEnd(true);
+        return;
+      }
+      updateRemainingTime();
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [remainingTime]);
 
-            `}</style>
-        </div>
-    )
-}
+  function updateRemainingTime() {
+    const date = new Date(remainTime);
+    const remainSec = ("0" + date.getSeconds()).slice(-2);
+    const remainMin = ("0" + date.getMinutes()).slice(-2);
+    const remainHour = Math.floor(remainTime / 1000 / 60 / 60);
+    const countdown = `${remainHour}:${remainMin}:${remainSec}`;
+    setRemainingTime(countdown);
+  }
 
-export default Countdown
+  return (
+    <div style={{ marginTop: 20 }}>
+      {remainTime < 1000 ? (
+        <div className="endAuction">경매 종료</div>
+      ) : (
+        <div>남은 시간 {remainingTime}</div>
+      )}
+      <style jsx>{`
+        div {
+          text-align: center;
+          color: #df973a;
+          font-weight: 700;
+        }
+        .endAuction {
+          font-size: 20px;
+          color: #999494;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Countdown;
