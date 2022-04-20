c339e3733dd1ff218b1d81ab4a33f801e6be0b34
codecocos <isawmy@nate.com>
2022-04-20 17:29:41 +0900

    chore: 주석제거

--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,5 @@
-import { applyMiddleware, compose, createStore, combineReducers } from "redux";
-import { HYDRATE, createWrapper } from "next-redux-wrapper";
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import { createWrapper } from "next-redux-wrapper";
 import thunk from "redux-thunk";
 import blockchainReducer from "./blockchain/blockchainReducer";
 import dataReducer from "./data/dataReducer";
@@ -11,23 +11,11 @@ const bindMiddleware = (middleware) => {
   }
   return applyMiddleware(...middleware);
 };
+
 const combinedReducer = combineReducers({
   blockchain: blockchainReducer,
   data: dataReducer,
 });
-
-// const reducer = (state, action) => {
-//   if (action.type === HYDRATE) {
-//     const nextState = {
-//       ...state, // use previous state
-//       ...action.payload, // apply delta from hydration
-//     };
-//     if (state.count.count) nextState.count.count = state.count.count; // preserve count value on client side navigation
-//     return nextState;
-//   } else {
-//     return combinedReducer(state, action);
-//   }
-// };
 
 const configureStore = () => {
   const store = createStore(combinedReducer, bindMiddleware([thunk]));
