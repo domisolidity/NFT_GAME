66b4ff923da3876bc9442ec4e1b0823502d0a41b
pier101 <85658044+pier101@users.noreply.github.com>
2022-04-25 22:02:00 +0900

    Build : build파일 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const Dotenv = require("dotenv-webpack");
-const nextBuildId = require("next-build-id");
 module.exports = {
   reactStrictMode: true,
-  generateBuildId: () => nextBuildId({ dir: __dirname, describe: true }),
   webpack: (config) => {
     // 기존의 웹팩 플러그인에 새로운 Dotenv플러그인을 연결시켜준다.
     // silent는 옵션은 .env파일을 찾지 못했을 때 에러를 일으키지 않도록 설정해주는 옵션이다.
