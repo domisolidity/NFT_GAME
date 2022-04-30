4b2def848ed83aa22b9df0b80a69a1719f16a901
pier101 <85658044+pier101@users.noreply.github.com>
2022-05-01 01:43:47 +0900

    delete cache

--- frontend/hooks/min.js
+++
@@ -1,7 +0,0 @@
-export function min( a,  b){
-    if (a <= b){
-        return a;
-    }else{
-        return b;
-    }
-}
--- frontend/theme/foundations/breakpoints.js
+++
@@ -1,7 +0,0 @@
-import { createBreakpoints } from "@chakra-ui/theme-tools";
-export const breakpoints = createBreakpoints({
-  sm: "320px",
-  md: "768px",
-  lg: "960px",
-  xl: "1200px",
-});
